b304c09a5f7b927a44b73929a606467dc1c853b7
Ben Proctor <ben@benproctor.co.uk>
2022-10-18 10:34:10 +0100

    Tidy and switches nicely between languages

--- wikidata/src/local_scripts.js
+++ wikidata/src/local_scripts.js
@@ -32,6 +32,41 @@ function generateSparqlQuery(category){
         `;
         return sparql_2 = chunk2
         break;
+        case "medical":
+          chunk2 = `SELECT ?place ?placeLabel ?location ?typeLabel ?operator ?operatorLabel WHERE {
+            SERVICE wikibase:box {
+                ?place wdt:P625 ?location .
+                bd:serviceParam wikibase:cornerWest "Point(${bounds.getWest()} ${bounds.getNorth()})"^^geo:wktLiteral .
+                bd:serviceParam wikibase:cornerEast "Point(${bounds.getEast()} ${bounds.getSouth()})"^^geo:wktLiteral .
+              }
+              FILTER EXISTS { ?place wdt:P31/wdt:P279* wd:Q4260475 }
+              ?place wdt:P31 ?type .
+             ?place wdt:P137 ?operator .
+             minus {?place  wdt:P31 wd:Q64578911 .} 
+             
+                SERVICE wikibase:label { bd:serviceParam wikibase:language "${lang_select}". }
+            } LIMIT 2000
+            `;
+            return sparql_2 = chunk2
+            break;
+            case "toilets":
+          chunk2 = `SELECT ?place ?placeLabel ?location ?typeLabel ?locationLabel WHERE {
+            SERVICE wikibase:box {
+                ?place wdt:P625 ?location .
+                bd:serviceParam wikibase:cornerWest "Point(${bounds.getWest()} ${bounds.getNorth()})"^^geo:wktLiteral .
+                bd:serviceParam wikibase:cornerEast "Point(${bounds.getEast()} ${bounds.getSouth()})"^^geo:wktLiteral .
+              }
+              FILTER EXISTS { ?place wdt:P31 wd:Q813966 }
+              ?place wdt:P31 ?type .
+             
+              
+             
+                SERVICE wikibase:label { bd:serviceParam wikibase:language "${lang_select}". }
+            } LIMIT 2000
+            `;
+            console.log("Ran the query")
+            return sparql_2 = chunk2
+            break;
     default:
       // Code block
  
@@ -43,55 +78,29 @@ function generateSparqlQuery(category){
 
 }
 
-function generateschoolsMarkers(){
-schools.clearLayers(); // Add the schools items to the Schools overlay
-      fetch("https://query.wikidata.org/sparql?query=" + encodeURIComponent(sparql_2), {
-        "headers": {
-          "accept": "application/sparql-results+json"
-        },
-        "method": "GET",
-        "mode": "cors"
-      }).then(a => a.json()).then(a => {
-        a.results.bindings.forEach(x => {
-          if (x.location.value.match(/^Point\((.+) (.+)\)$/)) {
-            var lon = parseFloat(RegExp.$1);
-            var lat = parseFloat(RegExp.$2);
-            var html =`<h3> ${x.placeLabel.value} </h3><span class='link_format'> <a href=\"https://www.openstreetmap.org/#map=16/${lat}/${lon}${linktext1OSM}${x.place.value}${linktext2OSM}`
-            if (x.placeLabel.value.match(/^Q[0-9]+$/)) {
-             //Do not add the point to the layer
-} else {
-           L.marker([lat, lon],{icon: schoolsmk}).bindPopup(html).openPopup().addTo(schools);
-           
-          }
-          } 
-        });
-      });
-    }
-
-function generatetransportMarkers(){
-    transport.clearLayers(); // Add the Trnaport items to the Schools overlay
-          fetch("https://query.wikidata.org/sparql?query=" + encodeURIComponent(sparql_2), {
-            "headers": {
-              "accept": "application/sparql-results+json"
-            },
-            "method": "GET",
-            "mode": "cors"
-          }).then(a => a.json()).then(a => {
-            a.results.bindings.forEach(x => {
-              if (x.location.value.match(/^Point\((.+) (.+)\)$/)) {
-                var lon = parseFloat(RegExp.$1);
-                var lat = parseFloat(RegExp.$2);
-                
-                var html =`<h3> ${x.placeLabel.value} </h3><span class='link_format'> <a href=\"https://www.openstreetmap.org/#map=16/${lat}/${lon}${linktext1OSM}${x.place.value}${linktext2OSM}`
-                
-                if (x.placeLabel.value.match(/^Q[0-9]+$/)) {
-                 //Do not add the point to the layer
-    } else {
-               L.marker([lat, lon],{icon: transportmk}).bindPopup(html).openPopup().addTo(transport);
-               
-              }
-              } 
-            });
+function generateMarkers(map_layer, marker_name){
+  map_layer.clearLayers(); // Add the toilets items to the toilets overlay
+        fetch("https://query.wikidata.org/sparql?query=" + encodeURIComponent(sparql_2), {
+          "headers": {
+            "accept": "application/sparql-results+json"
+          },
+          "method": "GET",
+          "mode": "cors"
+        }).then(a => a.json()).then(a => {
+          a.results.bindings.forEach(x => {
+            if (x.location.value.match(/^Point\((.+) (.+)\)$/)) {
+              var lon = parseFloat(RegExp.$1);
+              var lat = parseFloat(RegExp.$2);
+              
+              var html =`<h3> ${x.placeLabel.value} </h3><span class='link_format'> <a href=\"https://www.openstreetmap.org/#map=16/${lat}/${lon}${linktext1OSM}${x.place.value}${linktext2OSM}`
+              
+              if (x.placeLabel.value.match(/^Q[0-9]+$/)) {
+               //Do not add the point to the layer
+  } else {
+             L.marker([lat, lon],{icon: marker_name}).bindPopup(html).openPopup().addTo(map_layer);
+             
+            }
+            } 
           });
-
+        });
 }
